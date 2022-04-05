ba8ba98ec0329325d231b6d37a1f7567eeded160
MdHamidUddin <hamiduddin2018@gmail.com>
2022-04-06 00:10:35 +0600

    UserList Done

---
+++ AskNLearn/AskNLearn-WebAPI/Controllers/AdminController.cs
@@ -0,0 +1,56 @@
+﻿using BLL.Entities;
+using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Script.Serialization;
+
+namespace AskNLearn_WebAPI.Controllers
+{
+    [EnableCors("*","*","*")]
+    public class AdminController : ApiController
+    {
+            //[HttpGet]
+            //[Route("api/User/{id}")]
+
+            //public HttpResponseMessage Get(int id)
+            //{
+            //    var st = AdminServices.Get(id);
+            //    return Request.CreateResponse(HttpStatusCode.OK, st);
+            //}
+
+        [HttpGet]
+        [Route("api/User/{id}")]
+        public HttpResponseMessage Get(int id)
+        {
+            var data = AdminServices.Get(id);
+            //var d = new JavaScriptSerializer().Deserialize<List<UsersModel>>(data);
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+        }
+
+        [HttpGet]
+        [Route("api/User/")]
+        public HttpResponseMessage Get()
+        {
+            var data = AdminServices.Get();
+            return Request.CreateResponse(HttpStatusCode.OK, data);
+
+        }
+
+
+
+
+
+
+        //public HttpResponseMessage UserList()
+        //{
+        //    var st = AdminServices.UserList();
+        //    return Request.CreateResponse(HttpStatusCode.OK, st);
+        //}
+
+    }
+}
---
+++ AskNLearn/BLL/Services/AdminServices.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using BLL.Entities;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace BLL.Services
+{
+    public class AdminServices
+    {
+        
+        //public static UsersModel Get(int id)
+        //{
+        //    JavaScriptSerializer js = new JavaScriptSerializer();
+
+        //    var u = AdminDataAccessFactory.GetUser().Get(id);
+        //    return new UsersModel()
+        //    {
+        //        uid = u.uid,
+        //        name = u.name,
+        //        username = u.username,
+        //        password = u.password,
+        //        email = u.email,
+        //        approval = u.approval,
+        //        dob = u.dob,
+        //        gender = u.gender,
+        //        userType = u.userType,
+        //        proPic = u.proPic,
+        //        dateTime = u.dateTime
+        //    };
+        //}
+
+        public static UsersModel Get(int id)
+        {
+
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<User, UsersModel>());
+            var mapper = new Mapper(config);
+            var u = AdminDataAccessFactory.GetUser().Get(id);
+            var Data = mapper.Map<UsersModel>(u);
+
+            //var data = js.Serialize(u);
+            return Data;
+        }
+
+        public static List<UsersModel> Get()
+        {
+
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<User, UsersModel>());
+            var mapper = new Mapper(config);
+            var u = AdminDataAccessFactory.GetUser().Get();
+            var Data = mapper.Map<List<UsersModel>>(u);
+
+
+            return Data;
+        }
+
+    }
+
+    
+}
